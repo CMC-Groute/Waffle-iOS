e5ad8a9b29cd356f3fd736c685d2ee10f8532bf2
spqjf12345 <spqjf12345@gmail.com>
2022-06-04 22:17:30 +0900

    feat SnapKit

---
+++ Pods/Target Support Files/Pods-Waffle-WaffleUITests/Pods-Waffle-WaffleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Waffle_WaffleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Waffle_WaffleUITests
+@end
---
+++ Pods/Target Support Files/Pods-WaffleTests/Pods-WaffleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WaffleTests : NSObject
+@end
+@implementation PodsDummy_Pods_WaffleTests
+@end
