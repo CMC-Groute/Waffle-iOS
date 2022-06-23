7e12314635672eca9ba266765aaf100d7559711d
jouureee <spqjf12345@gmail.com>
2022-06-23 20:38:54 +0900

    Update images

---
+++ Waffle/Presentation/HomeScene/ViewController/Cell/DetailPlaceTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DetailPlaceTableViewCell.swift
+//  Waffle
+//
+//  Created by 조한빛 on 2022/06/23.
+//
+
+import UIKit
+
+class DetailPlaceTableViewCell: UITableViewCell {
+    static var identifier = "DetailPlaceTableViewCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
