791b9054129e39742719eda43864ba9a6d728a68
jouureee <spqjf12345@gmail.com>
2022-06-25 18:57:48 +0900

    Update .normal 일때 스크롤 안되게 셋팅

--- Waffle/Presentation/HomeScene/ViewController/PopUp/Button/DetailPlacePopUpViewController.swift
+++ Waffle/Presentation/HomeScene/ViewController/PopUp/Button/DetailPlacePopUpViewController.swift
@@ -15,8 +15,10 @@ class DetailPlacePopUpViewController: UIViewController {
     }
     
     @IBOutlet private weak var bottomSheetView: UIView!
+    @IBOutlet private weak var scrollView: UIScrollView!
     @IBOutlet private weak var topConstraint: NSLayoutConstraint!
     @IBOutlet private weak var draggingView: UIView!
+    
     var bottomSheetPanMinTopConstant: CGFloat = 34
     var defaultHeight:CGFloat = 395
     private lazy var bottomSheetPanStartingTopConstant: CGFloat = bottomSheetPanMinTopConstant
@@ -33,19 +35,19 @@ class DetailPlacePopUpViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
-        // Pan Gesture Recognizer를 view controller의 view에 추가하기 위한 코드
-        let viewPan = UIPanGestureRecognizer(target: self, action: #selector(viewPanned(_:)))
-        
-        // 기본적으로 iOS는 터치가 드래그하였을 때 딜레이가 발생함
-        // 우리는 드래그 제스쳐가 바로 발생하길 원하기 때문에 딜레이가 없도록 아래와 같이 설정
-        viewPan.delaysTouchesBegan = false
-        viewPan.delaysTouchesEnded = false
-        view.addGestureRecognizer(viewPan)
     }
     
     private func configureUI() {
         draggingView.round(width: nil, color: nil, value: 3)
         bottomSheetView.roundCorners(value: 20, maskedCorners: [.layerMinXMinYCorner, .layerMaxXMinYCorner])
+        configureGesture()
+    }
+    
+    private func configureGesture() {
+        let viewPan = UIPanGestureRecognizer(target: self, action: #selector(viewPanned(_:)))
+        viewPan.delaysTouchesBegan = false
+        viewPan.delaysTouchesEnded = false
+        bottomSheetView.addGestureRecognizer(viewPan)
     }
     
     @objc private func viewPanned(_ panGestureRecognizer: UIPanGestureRecognizer) {
@@ -81,10 +83,12 @@ class DetailPlacePopUpViewController: UIViewController {
     
     private func showBottomSheet(atState: BottomSheetViewState = .normal) {
         if atState == .normal {
+            scrollView.isScrollEnabled = false
             let safeAreaHeight: CGFloat = view.safeAreaLayoutGuide.layoutFrame.height
             let bottomPadding: CGFloat = view.safeAreaInsets.bottom
             topConstraint.constant = (safeAreaHeight + bottomPadding) - defaultHeight
         } else {
+            scrollView.isScrollEnabled = true
             topConstraint.constant = bottomSheetPanMinTopConstant
         }
         
