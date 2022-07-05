3b1906df9308e304f2db60a8983a839d3ed252e0
jouureee <spqjf12345@gmail.com>
2022-07-05 19:42:22 +0900

    Add ArchiveAPI

---
+++ Waffle/Data/Repositories/API/ArchiveAPI.swift
@@ -0,0 +1,71 @@
+//
+//  ArchiveAPI.swift
+//  Waffle
+//
+//  Created by 조한빛 on 2022/07/05.
+//
+
+import Foundation
+
+enum ArchiveAPI: NetworkRequestBuilder {
+    case makeArchive(archiveInfo: ArchiveInfo)
+    case joinArchive(code: String)
+    case getArchiveCode(archiveId: Int)
+    case getArchiveCard
+    case getArchiveDetail(archiveId: Int)
+    case deleteArchive(archiveId: Int)
+}
+
+extension ArchiveAPI {
+    var baseURL: URL? {
+        return URL(string: Config.baseURL)
+    }
+    
+    var path: String? {
+        switch self {
+        case .makeArchive(_), .getArchiveCard:
+            return "/invitations"
+        case .joinArchive(_):
+            return "/invitations/code"
+        case .getArchiveCode(let id):
+            return "/invitations/\(id)/code"
+        case .getArchiveDetail(let id):
+            return "/invitations/\(id)"
+        case .deleteArchive(let id):
+            return "/invitations/\(id)/invitationMember"
+        }
+    }
+    
+    var parameters: [String : String]? {
+        return nil
+    }
+    
+    var method: HttpMethod {
+        switch self {
+        case .getArchiveCard, .getArchiveDetail(_), .getArchiveCode(_):
+            return .get
+        case .joinArchive(_), .makeArchive(_):
+            return .post
+        case .deleteArchive(_):
+            return .delete
+        }
+    }
+    
+    var headers: [String : String]? {
+        let defaultHeader = ["Content-Type" : "application/json"]
+        guard let jwtToken = UserDefaults.standard.string(forKey: UserDefaultKey.jwtToken) else { return defaultHeader }
+        return ["token": jwtToken, "Content-Type" : "application/json"]
+    }
+    
+    var body: [String : Any]? {
+        switch self {
+            case .getArchiveCard, .getArchiveCode(_), .getArchiveDetail(_), .deleteArchive(_):
+                return nil
+            case .joinArchive(let code):
+                return code.dictionary
+            case .makeArchive(let archive):
+                return archive.dictionary
+            }
+    }
+    
+}
