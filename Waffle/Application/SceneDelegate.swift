931b535bd963b79ed5a7037f7e492e22e0011b39
jouureee <spqjf12345@gmail.com>
2022-07-14 23:03:36 +0900

    Add: 초대장 클릭시 detailArchive로 화면 이동

--- Waffle/Application/SceneDelegate.swift
+++ Waffle/Application/SceneDelegate.swift
@@ -52,11 +52,30 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
     
-    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
-        if let url = URLContexts.first?.url {
-            //example: kakao9d221cbc36f57f5d7e31879b43c6a546://kakaolink?key1=value1&key2=value2
-            print("url : \(url)")
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {    
+        if let url = URLContexts.first?.url, UserDefaults.standard.string(forKey: UserDefaultKey.jwtToken) != nil {
+            //example: kakao9d221cbc36f57f5d7e31879b43c6a546://kakaolink?archiveId=19
+
+            var components = URLComponents(url: url, resolvingAgainstBaseURL: false)!
+            print(components.queryItems?.first?.name) // archiveId
+            let archiveId = components.queryItems?.first?.value ?? "0"
+            print(components.queryItems?.first?.value) // 19
+            guard let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate,
+                  let appCoordinator = sceneDelegate.appCoordinator,
+                  let tabBarCoordinator = appCoordinator.findCoordinator(type: .tab) as? TabBarCoordinator,
+                  let homeCoordinator = appCoordinator.findCoordinator(type: .home) as? HomeCoordinator else { return }
+            tabBarCoordinator.selectPage(.home)
+            print("homeCoordinator.navigationController.viewControllers.last \(homeCoordinator.navigationController.viewControllers.last)")
+            guard (homeCoordinator.navigationController.viewControllers.last
+                   is DetailArchiveViewController == false) else { return }
+            //마지막 화면이 DetailArchiveViewController가 아니였다면
+            homeCoordinator.detailArchive(archiveId: Int(archiveId)!)
+            
         }
+//        else { // Login 화면으로 이동
+//            self.appCoordinator = AppCoordinator(navigationController)
+//            appCoordinator?.start()
+//        }
     }
 
 
