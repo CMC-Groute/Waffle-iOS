f5d7e046f06403a9b978dc116128bccb9ba61266
jouureee <spqjf12345@gmail.com>
2022-07-18 21:38:50 +0900

    Add: 알림 조회 Entity 추가

--- Waffle/Domain/Entities/Alarm/Alarm.swift
+++ Waffle/Domain/Entities/Alarm/Alarm.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct GetAlarm: Codable {
+    var status: Int
+    var data: [Alarm]
+}
+
+struct Alarm: Codable {
+    var archiveId: Int
+    var archiveTitle: String
+    var nickName: String
+    var pushType: String
+    
+    enum CodingKeys: String, CodingKey {
+        case archiveId = "invitationId"
+        case archiveTitle = "invitationTitle"
+        case nickName, pushType
+    }
+}
